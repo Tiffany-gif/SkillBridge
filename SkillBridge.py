f2cb2cdea70b591775b736e7a4df33f6f0f5088e
Your Name <you@example.com>
2025-03-31 00:47:38 -0700

    Added a function that lets the user post a job

--- SkillBridge.py
+++ SkillBridge.py
@@ -36,3 +36,74 @@ CATEGORIES = [
     "Writing", "Programming", "Design", "Marketing", "Consulting",
     "Photography", "Music", "Teaching", "Handyman", "Other"
 ]
+
+# Function posts a job and saves it into the SkillBridge database
+def post_job():
+    # Validate title input 
+    while True:
+        try:
+            title = str(input("Job title: ")).strip()
+            if len(title) == 0:
+                print("Please enter a job title.")
+            else:
+                break 
+            except ValueError:
+                print("Invalid input. Please enter valid text title.")
+    # Validate description input 
+    while True:
+        try:
+            description = str(input("Description: ")).strip()
+            if len(description) == 0:
+                print("Please enter a job description.")
+            else:
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid text validation.")
+    # Validate pay input
+    while True:
+        try:
+            pay = input("Pay in USD: ").strip()
+            pay = float(pay)
+            if pay < 0:
+                print("Pay cannot be negative.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    # Validate contact input
+    while True:
+        try:
+            contact = str(input("Contact info: ")).strip()
+            if len(contact) == 0:
+                print("Please enter a contact.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid contact. Email or Phone.")
+    # Validate location
+    while True:
+        try:
+            location = str(input("Location: ")).strip()
+            if len(contact) == 0:
+                print("Please enter a job location.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid text location.")
+    # Get validated category input
+    category = get_valid_category()
+
+    # Validate job date input
+    while True:
+        job_date = input("Enter job date and time (YYYY-MM-DD HH:MM): ").strip()
+        try:
+            job_date = datetime.datetime.strptime(job_date, "%Y-%m-%d %H:%M")
+            break
+        except ValueError:
+            print("Invalid date format. Please enter in YYYY-MM-DD HH:MM format.")
+
+    job_id = generate_job_id()
+    cursor.execute("INSERT INTO jobs VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                   (job_id, title, description, pay, contact, location, category, job_date))
+    conn.commit()
+    print(f"Job posted! Save this job ID for management: {job_id}")
